4c485e33c0987a5d490e3305e76bf09663815845
Luka Golubovic <62186386+golubovicluka@users.noreply.github.com>
2023-03-19 00:55:28 +0100

    created product service

---
+++ src/app/features/products/Product.ts
@@ -0,0 +1,8 @@
+export interface Product {
+  id: number
+  name: string
+  description: string
+  rating: number
+  createdAt: Date
+  category: string
+}
--- src/app/features/products/products.service.ts
+++ src/app/features/products/products.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { Product } from './Product';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductsService {
+  products: Product[] = [
+    { id: 1, name: "Mechanical keyboard", description: "A new mechanical custom built keyboard", createdAt: new Date(), rating: 5, category: "PC Components" },
+    { id: 2, name: "Gaming mouse", description: "Brand new gaming mouse", createdAt: new Date(), rating: 4, category: "PC Components" },
+    { id: 3, name: "Gaming monitor", description: "144hz gaming monitor", createdAt: new Date(), rating: 5, category: "PC Components" },
+  ]
 
   constructor() { }
+
+  getProducts(): Observable<Product[]> {
+    return of(this.products);
+  }
+
 }
