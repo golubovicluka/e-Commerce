9371052cdfcc7c63276f2f9c2dbe8b697edee409
Luka Golubovic <62186386+golubovicluka@users.noreply.github.com>
2023-04-09 01:01:01 +0200

    add home page products overview section ui and functionality

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  iphoneImg = "https://istyle.rs/media/catalog/product/cache/image/700x700/e9c3970ab036de70892d86c6d221abfe/c/z/czcs_iphone14promax_q422_space-black_pdp-images_position-1a_t_4_7.jpg";
+  iphoneId = 4;
+
+  macbookImg = "https://img.gigatron.rs/img/products/large/image62becf4b4c714.png";
+  macbookId = 1;
+
+  ipadImg = "https://istyle.rs/media/catalog/product/cache/image/700x700/e9c3970ab036de70892d86c6d221abfe/i/p/ipad_pro_q123_wi-fi_11_in_4th_generation_silver_pdp_image_position-1a__wwce_t_t_4.jpg";
+  ipadId = 13;
+
+  constructor(private router: Router){ }
+
+  ngOnInit(){
+
+  }
+
+  redirect(id: number){
+    this.router.navigate(['/products', id]);
+  }
 
 }
--- src/app/features/home/home.module.ts
+++ src/app/features/home/home.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
-
+import { ButtonModule } from 'primeng/button';
 
 @NgModule({
   declarations: [
@@ -11,7 +11,8 @@ import { HomeComponent } from './home.component';
   ],
   imports: [
     CommonModule,
-    HomeRoutingModule
+    HomeRoutingModule,
+    ButtonModule
   ]
 })
 export class HomeModule { }
