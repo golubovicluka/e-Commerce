0bb9ab93b01e8a9cdea12ae9e4edeb497689c3ff
Luka Golubovic <62186386+golubovicluka@users.noreply.github.com>
2023-06-09 23:01:25 +0200

    feature: added header e2e tests

--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -1,3 +1,4 @@
+// Home page tests
 describe("Home page tests", () => {
     beforeEach(() => {
         cy.visit("http://localhost:4200/home")
@@ -37,6 +38,7 @@ describe("Home page tests", () => {
     })
 })
 
+// Footer tests
 describe('Footer menu tests', () => {
     beforeEach(() => {
         cy.visit('http://localhost:4200/home')
@@ -79,4 +81,41 @@ describe('Footer menu tests', () => {
     it('should have visible GitHub icon', () => {
         cy.get('.pi-github').should('exist')
     })
+})
+
+// Header tests
+describe('Header menu tests', () => {
+    beforeEach(() => {
+        cy.visit('http://localhost:4200/home')
+    })
+
+    it("should navigate to Home page on clicking Home link", () => {
+        cy.get(".header li").contains("Home").click();
+        cy.url().should("include", "/home");
+    });
+
+    it("should navigate to Products page on clicking Products link", () => {
+        cy.get(".header li").contains("Products").click();
+        cy.url().should("include", "/products/search");
+    });
+
+    it("should navigate to Categories page on clicking Categories link", () => {
+        cy.get(".header li").contains("Categories").click();
+        cy.url().should("include", "/categories");
+    });
+
+    it("should navigate to Login page on clicking Login link", () => {
+        cy.get(".header li").contains("Login").click();
+        cy.url().should("include", "/login");
+    });
+
+    it("should navigate to Wishlist page on clicking Wishlist link", () => {
+        cy.get(".header li").contains("Wishlist").click();
+        cy.url().should("include", "/wishlist");
+    });
+
+    it("should navigate to Cart page on clicking Cart link", () => {
+        cy.get(".header li").contains("Cart").click();
+        cy.url().should("include", "/cart");
+    });
 })
