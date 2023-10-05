4200209589b52bbcb672fab1937a4b6d1ff175b9
Luka Golubovic <62186386+golubovicluka@users.noreply.github.com>
2023-10-05 12:28:52 +0200

    feat(cart): refactored cart service methods, added getTotalPrice & getNumberOfItems in cart

--- src/app/shared/cart.service.ts
+++ src/app/shared/cart.service.ts
@@ -1,39 +1,55 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, Observable } from 'rxjs';
+import {BehaviorSubject, map, Observable} from 'rxjs';
 import { Product } from '../features/products/Product';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CartService {
-  // TODO: create DTO to contain Product + amount of items in the cart of that type
   private cartItems$ = new BehaviorSubject<Product[]>([]);
   cartItems = this.cartItems$.asObservable();
 
   constructor() {
     const cartItems = localStorage.getItem('cart');
-    if (cartItems) {
-      this.cartItems$.next(JSON.parse(cartItems));
-    }
+    if (cartItems) this.cartItems$.next(JSON.parse(cartItems));
   }
 
   addToCart(item: Product) {
     const currentItems = [...this.cartItems$.value, item];
+    const serializedItems = JSON.stringify(currentItems);
 
-    localStorage.setItem('cart', JSON.stringify(currentItems));
+    try {
+      localStorage.setItem('cart', serializedItems);
+    } catch (error) {
+      console.error('Failed to save cart items to localStorage:', error);
+      return;
+    }
+
     this.cartItems$.next(currentItems);
   }
 
   removeFromCart(item: Product) {
-    let currentItems = this.cartItems$.value;
-    currentItems = currentItems.filter(product => product.id !== item.id);
+    const currentItems = this.cartItems$.value.filter(product => product?.id !== item?.id);
     localStorage.setItem('cart', JSON.stringify(currentItems));
-
     this.cartItems$.next(currentItems);
   }
 
   getCartItems(): Observable<Product[]> {
     return this.cartItems;
+  }
+
+  getTotalPrice(): Observable<number> {
+    return this.cartItems$.pipe(
+      map((products) =>
+        products.reduce((total, product) => total + product.price, 0)
+      )
+    );
+  }
+
+  getNumberOfItems(): Observable<number> {
+    return this.cartItems$.pipe(
+      map((products) => products.length)
+    );
   }
 
   inCart(id: number) {
