778af09c33227ddd2d44d6d30756863c909c47db
golubovicluka <luka_golubovic@yahoo.com>
2024-08-25 11:55:01 +0200

    fix(config): remove unecessary files

--- src/environments/environment.prod.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-    production: true,
-    hasuraAdminSecret: '6sftAV4UtDQ6V26v1p4U4mDAS8eXiDDnBo62JFsQbdTjksQQjcF54reBmrA2p7Jl'
-  };
--- src/environments/environment.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-    production: false,
-    hasuraAdminSecret: '6sftAV4UtDQ6V26v1p4U4mDAS8eXiDDnBo62JFsQbdTjksQQjcF54reBmrA2p7Jl'
-  };
